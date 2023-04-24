f8320417712b63a66af30e8996f50088df8e6e2c
Adil Sadigov <sadiqovadil9@gmail.com>
2023-04-24 15:26:50 +0300

    added exercise

---
+++ dist/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import morgan from 'morgan';
+import "express-async-errors";
+import "dotenv/config";
+const app = express();
+const port = process.env.PORT;
+let planets = [
+    {
+        id: 1,
+        name: "Earth",
+    },
+    {
+        id: 2,
+        name: "Mars",
+    },
+];
+app.use(morgan("dev"));
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.status(200).json(planets);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});

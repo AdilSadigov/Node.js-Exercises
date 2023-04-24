58fae983969d3116b19d9e5b2f94e238477111f4
Adil Sadigov <sadiqovadil9@gmail.com>
2023-04-24 16:31:45 +0300

    added exercise

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import joi from 'joi';
 import "express-async-errors";
 import "dotenv/config";
 const app = express();
@@ -14,10 +15,48 @@ let planets = [
         name: "Mars",
     },
 ];
+const schema = joi.object({
+    id: joi.when(joi.ref('$method'), {
+        is: 'POST',
+        then: joi.number().required(),
+        otherwise: joi.number()
+    }),
+    name: joi.string().min(3).required()
+});
 app.use(morgan("dev"));
 app.use(express.json());
-app.get('/', (req, res) => {
+app.get('/api/planets', (req, res) => {
     res.status(200).json(planets);
+});
+app.get('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    const planet = planets.find(p => p.id === Number(id));
+    res.status(200).json(planet);
+});
+app.post('/api/planets', (req, res) => {
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+    const { id, name } = req.body;
+    const newPlanet = { id, name };
+    planets = [...planets, newPlanet];
+    res.status(201).json({ msg: "The planet was created" });
+});
+app.put('/api/planets/:id', (req, res) => {
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+    const { id } = req.params;
+    const { name } = req.body;
+    planets = planets.map(p => p.id === Number(id) ? (Object.assign(Object.assign({}, p), { name })) : p);
+    res.status(200).json({ msg: "The planet was updated" });
+});
+app.delete('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    planets = planets.filter((p) => p.id !== Number(id));
+    res.status(200).json({ msg: "The planet was deleted" });
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
